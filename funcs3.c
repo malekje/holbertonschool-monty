bf79d23eff427a09dce50cbe94fde01faf1bb6df
Malek Mayeh <loukatankionline@gmail.com>
2022-08-19 15:41:00 +0100

    funcs3 nop

---
+++ funcs3.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * nop - doesn't do anything
+ * @stack: pointer
+ * @line_number: line of the file
+ * Return: void
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
