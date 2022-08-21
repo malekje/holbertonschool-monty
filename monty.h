c5118e6e9f34c859309d01e8fd1455530f198b62
Malek Mayeh <loukatankionline@gmail.com>
2022-08-21 16:57:47 +0100

    header file

---
+++ monty.h
@@ -0,0 +1,60 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+#define STACK 1
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+char *value;
+stack_t *stackit = NULL;
+void get_op_func(char *func_name, unsigned int line_number, stack_t **stack);
+int main(int argc, char **argv);
+int positive(char *func_name);
+void push(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void freeit(stack_t **stack);
+void add_(stack_t **stack, unsigned int line_count);
+void pop(stack_t **stack, unsigned int line_count);
+void pall(stack_t **stack, unsigned int line_number);
+int is_digit(char *arg);
+void nop(stack_t **stack, unsigned int line_number);
+void execute_func(char *line, unsigned int line_number, stack_t **stack);
+void swap(stack_t **stack, unsigned int line_number);
+
+#endif
